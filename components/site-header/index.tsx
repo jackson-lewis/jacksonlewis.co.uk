b71407c32e3cb13aff7c25f0b8af187732a5cacd
Jackson Lewis <hello@jacksonlewis.co.uk>
2023-09-17 20:40:22 +0100

    Removed site nav

--- components/site-header/index.tsx
+++ components/site-header/index.tsx
@@ -13,35 +13,8 @@ export default function SiteHeader() {
             <header className={styles.header}>
                 <div className={`container ${styles.container}`}>
                     <Link href="/">Jackson</Link>
-                    <nav className={styles.nav}>
-                        <ul role="menu">
-                            <li role="menuitem">
-                                <Link href="/work">Work</Link>
-                            </li>
-                            <li role="menuitem">
-                                <Link href="/life">Life</Link>
-                            </li>
-                            <li role="menuitem">
-                                <Link href="/contact">Contact</Link>
-                            </li>
-                        </ul>
-                    </nav>
-                    <button
-                        type="button"
-                        className={[styles['mobile-menu-trigger'], isToggled ? styles.active : ''].filter(Boolean).join(' ')}
-                        onClick={() => updateToggleState(!isToggled)}
-                    >
-                        <svg width="18" height="15" viewBox="0 0 18 15">
-                            <title>Open mobile navigation</title>
-                            <g fill="#fff">
-                                <rect width="18" height="1" y="3" />
-                                <rect width="18" height="1" y="11" />
-                            </g>
-                        </svg>
-                    </button>
                 </div>
             </header>
-            <MobileMenu isToggled={isToggled} />
         </div>
     )
 }
