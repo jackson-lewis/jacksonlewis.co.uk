f3d0979e608af8fe9e849c1c0bf2406eb29c8e83
Jackson Lewis <hello@jacksonlewis.co.uk>
2025-04-19 18:34:37 +0100

    added missing webvitals component import

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import SiteHeader from '../components/site-header'
 import SiteFooter from '../components/site-footer'
 import {roboto, robotoCondensed, robotoSerif} from './fonts'
 import { GoogleAnalytics } from '@next/third-parties/google'
+import { WebVitals } from '../components/web-vitals'
 
 export const metadata: Metadata = {
   title: 'Jackson Lewis',
@@ -21,6 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en-gb">
       <GoogleAnalytics gaId="G-5PKSSSCT6E" debugMode={!!process.env.VERCEL} />
+      <WebVitals />
       <body className={`${roboto.variable} ${robotoSerif.variable} ${robotoCondensed.variable}`}>
         <SiteHeader />
         {children}
